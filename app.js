bb65fa87a618bd2f69832eae0634ef7d1d7be150
Gayan C Liyanage <elinkliyanage@gmail.com>
2015-10-01 15:59:43 +0800

    change ...

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('hello jenkins');
+    function formatAMPM() {
+        var d = new Date(),
+            minutes = d.getMinutes().toString().length == 1 ? '0'+d.getMinutes() : d.getMinutes(),
+            hours = d.getHours().toString().length == 1 ? '0'+d.getHours() : d.getHours(),
+            ampm = d.getHours() >= 12 ? 'pm' : 'am',
+            months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+            days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+        return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
+    }
+    res.send(formatAMPM());
 });
 
 app.listen(process.env.PORT || 5000);
