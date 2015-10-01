64810c085e2007ad0b44a682c6c1c1056c5bb165
Gayan C Liyanage <elinkliyanage@gmail.com>
2015-10-01 10:54:02 +0800

    Add node app

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by gayancliyanage on 1/10/15.
+ */
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
+
+app.listen(process.env.PORT || 5000);
+
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by gayancliyanage on 1/10/15.
+ */
+var request = require('supertest');
+var app = require('../app.js');
+
+describe('GET /', function() {
+    it('respond with hello world', function(done) {
+        request(app).get('/').expect('hello world', done);
+    });
+});
